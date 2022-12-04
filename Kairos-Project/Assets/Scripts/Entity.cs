e0733f4c5ddae51184652d030e7e3e753da425eb
Noah Gallo <noah.gallo@live.com>
2022-12-03 23:08:24 -0800

    Implemented simple health and damage system

--- Kairos-Project/Assets/Scripts/Entity.cs
+++ Kairos-Project/Assets/Scripts/Entity.cs
@@ -13,6 +13,12 @@ public abstract class Entity : MonoBehaviour
     public KeyCode hotkey;
     public float rotateSpeed = 10f;
 
+    //Health
+    [field:SerializeField] public float MaxHealth { get; private set; }
+    [field:SerializeField] public float Health { get; private set; }
+    public float HealthRatio { get { return Mathf.Clamp(Health / MaxHealth, 0, 1); } }
+    [field: SerializeField] public bool Invulnerable { get; private set; }
+
     public void SetSelectedVisible(bool selected)
     {
         if(selectedHighlight != null)
@@ -33,13 +39,19 @@ public abstract class Entity : MonoBehaviour
         GameController.main.playerController.playerEntities.Remove(gameObject);
     }
 
+    /// <summary>
+    /// Damages the entity. Pass this function the raw damage value, and it will determine how much health to subtract from the entity
+    /// <para>**Currently just removes the raw damage amount from health**</para> 
+    /// </summary>
+    /// <param name="damage">The raw damage value</param>
+    public void DamageEntity(float damage)
+    {
+        if(!Invulnerable)
+            Health -= damage;
+    }
+
     protected void RotateTowards(Vector3 pos)
     {
-        /*
-        if (lockHorizontalRotation)
-        {
-            pos.y = transform.position.y;
-        }*/
 
         pos.y = transform.position.y;
         Quaternion rotation = transform.rotation;
