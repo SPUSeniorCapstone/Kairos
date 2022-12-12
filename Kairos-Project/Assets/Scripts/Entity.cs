be9348a9f4139ad8482dee18054e8c591894e3b1
Noah Gallo <gallon1@spu.edu>
2022-12-11 23:24:39 -0800

    Revert "Delete Entity.cs"
    This reverts commit eab4cccced1a0db3fa7fa8a0393fbb16f65142c0.

---
+++ Kairos-Project/Assets/Scripts/Entity.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The base class from which all units and structures inherit
+/// </summary>
+public abstract class Entity : MonoBehaviour
+{
+    [SerializeField]
+    GameObject selectedHighlight;
+    public bool select = false;
+    public KeyCode hotkey;
+    public float rotateSpeed = 10f;
+
+    public void SetSelectedVisible(bool selected)
+    {
+        if(selectedHighlight != null)
+        {
+            selectedHighlight.SetActive(selected);
+            select = selected;
+        }
+    }
+
+    protected void Start()
+    {
+        //!DELETE
+        GameController.main.playerController.playerEntities.Add(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        GameController.main.playerController.playerEntities.Remove(gameObject);
+    }
+
+    protected void RotateTowards(Vector3 pos)
+    {
+        /*
+        if (lockHorizontalRotation)
+        {
+            pos.y = transform.position.y;
+        }*/
+
+        pos.y = transform.position.y;
+        Quaternion rotation = transform.rotation;
+
+        Vector3 direction = pos - transform.position;
+        var lookRotation = Quaternion.LookRotation(direction);
+
+
+        rotation = Quaternion.Slerp(rotation, lookRotation, Time.deltaTime * rotateSpeed);
+        transform.rotation = rotation;
+    }
+}
