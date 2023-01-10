f71c4adfb2dcff0db31075ff154af396885a7b6e
toughmuffin12 <nelsonc8@spu.edu>
2023-01-10 11:34:47 -0800

    Create Pause Menu
    Bare Bones Pause Menu which needs to be reviewed

--- Kairos-Project/Assets/Scripts/User Interface/MenuController.cs
+++ Kairos-Project/Assets/Scripts/User Interface/MenuController.cs
@@ -5,6 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
     public void PlayGame ()
     {
         SceneManager.LoadScene("Game");
