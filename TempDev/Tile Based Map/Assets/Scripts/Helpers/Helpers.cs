93e8b78e7eb93bdf0b715410bc8f0be7af07672a
Noah Gallo <gallon1@spu.edu>
2023-01-16 17:03:41 -0800

    Attempted tile based map

---
+++ TempDev/Tile Based Map/Assets/Scripts/Helpers/Helpers.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helpers
+{
+    public static Vector2Int ToVector2Int(Vector2 vec)
+    {
+        Vector2Int ret = new Vector2Int();
+        ret.x = Mathf.FloorToInt(vec.x);
+        ret.y = Mathf.FloorToInt(vec.y);
+        return ret;
+    }
+
+    public static Vector2Int ToVector2Int(Vector3Int vec)
+    {
+        Vector2Int ret = new Vector2Int();
+        ret.x = vec.x;
+        ret.y = vec.z;
+        return ret;
+    }
+
+    public static Vector3 Vector2IntToVector3(Vector2Int vec, float y = 0)
+    {
+        Vector3 ret = new Vector3();
+        ret.x = vec.x;
+        ret.y = y;
+        ret.z = vec.y;
+        return ret;
+    }
+}
