01cd6104392929a07db18f62b570f9e18acd4c49
Noah Gallo <noah.gallo@live.com>
2023-01-26 21:44:21 -0800

    Added input controller

--- Kairos/Assets/Scripts/InputController.cs
+++ Kairos/Assets/Scripts/InputController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,66 @@ using UnityEngine;
 public class InputController
 {
 
+    public InputType MoveForward = new InputType(KeyCode.W, KeyCode.UpArrow);
+    public InputType MoveBack = new InputType(KeyCode.S, KeyCode.DownArrow);
+    public InputType MoveRight = new InputType(KeyCode.D, KeyCode.RightArrow);
+    public InputType MoveLeft = new InputType(KeyCode.A, KeyCode.LeftArrow);
+
+
+    [Serializable]
+    public struct InputType
+    {
+        public enum HoldKey
+        {
+
+        }
+
+        KeyCode key1;
+        KeyCode key2;
+
+        
+
+        public InputType(KeyCode primary, KeyCode secondary)
+        {
+            key1 = primary;
+            key2 = secondary;
+        }
+
+        /// <summary>
+        /// returns true during the frame the key is pressed
+        /// </summary>
+        public bool Down()
+        {
+            if(Input.GetKeyDown(key1) || Input.GetKeyDown(key2))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// returns true while the key is held down
+        /// </summary>
+        public bool Pressed()
+        {
+            if(Input.GetKey(key1) || Input.GetKey(key2))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// returns true during the frame the user releases the key
+        /// </summary>
+        public bool KeyUp()
+        {
+            if (Input.GetKeyUp(key1) || Input.GetKeyUp(key2))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
 }
+
