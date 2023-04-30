cd18ef6750221f03110f5420152219b1ea38b839
Noah Gallo <noah.gallo@live.com>
2023-04-29 23:28:21 -0700

    Enhanced Corruption Spread
    New point-source spread
    New vein spread

--- Kairos/Assets/Scripts/Corruption/CorruptionController.cs
+++ Kairos/Assets/Scripts/Corruption/CorruptionController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class CorruptionController : MonoBehaviour
 {
+    public enum CorruptionMode { AVERAGE_SPREAD, POINT_SPREAD }
+
+    public CorruptionMode corruptionMode = CorruptionMode.AVERAGE_SPREAD;
+
     [Range(0,1)]
     public float corruptionStrength = 0.1f;
 
@@ -18,6 +24,16 @@ public class CorruptionController : MonoBehaviour
 
     public bool SpawnUnits = true;
 
+
+    public bool DoCorruptionVeins = true;
+    [Range (0, 1000)]
+    public int veinTick = 1;
+    public int maxVeins = 10;
+    public int maxStepsPerVein = 10;
+
+    [Range(0, 100)]
+    public float veinChance = 1;
+
     private void Start()
     {
         //var max = new Vector2Int(WorldController.Main.World.WidthInBlocks, WorldController.Main.World.LengthInBlocks);
@@ -30,10 +46,51 @@ public class CorruptionController : MonoBehaviour
 
     private void Update()
     {
-        CorruptionUpdate();
+        if(corruptionMode == CorruptionMode.AVERAGE_SPREAD)
+        {
+            AverageSpreadUpdate();
+        }
+        else
+        {
+            PointSpreadUpdate();
+        }
+
+        if (DoCorruptionVeins)
+        {
+            CorruptionVeins();
+        }
     }
 
-    public void CorruptionUpdate()
+    public void PointSpreadUpdate()
+    {
+        foreach (Chunk c in WorldController.Main.World.Chunks)
+        {
+            var positions = GetRandomPositions(RandomTick, Vector2Int.zero, new Vector2Int(Chunk.width, Chunk.length));
+            foreach (var position in positions)
+            {
+                var pos = new Vector2Int(c.Position.x * Chunk.width, c.Position.z * Chunk.length) + position;
+
+                var corr = WorldController.Main.World.GetCorruption(pos.x, pos.y);
+
+                if (corr < minimumCorruptionSpread)
+                {
+                    continue;
+                }
+
+                foreach (var point in GetNeighbors(pos.x, pos.y))
+                {
+                    var currCorr = WorldController.Main.World.GetCorruption(point.x, point.y);
+                    WorldController.Main.World.SetCorruption(point.x, point.y, currCorr + corr * corruptionStrength * Time.deltaTime);
+                }
+
+            }
+
+
+
+        }
+    }
+
+    public void AverageSpreadUpdate()
     {
         foreach (Chunk c in WorldController.Main.World.Chunks)
         {
@@ -75,6 +132,34 @@ public class CorruptionController : MonoBehaviour
         return strength/c;
     }
 
+    List<Vector2Int> GetNeighbors(int x, int z)
+    {
+        List<Vector2Int> neighbors = new List<Vector2Int>();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (j == 0 && i == 0)
+                    continue;
+
+                if(IsValidPosition(x + i, z + j))
+                {
+                    neighbors.Add(new Vector2Int(x + i, z + j));
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    bool IsValidPosition(int x, int z)
+    {
+        if(x < 0 || x > WorldController.Main.World.WidthInBlocks || z < 0 || z > WorldController.Main.World.LengthInBlocks)
+        {
+            return false;
+        }
+        return true;
+    }
+
     List<Vector2Int> GetRandomPositions(int count, Vector2Int min, Vector2Int max)
     {
         List<Vector2Int> positions = new List<Vector2Int>();
@@ -89,4 +174,48 @@ public class CorruptionController : MonoBehaviour
         return positions;
     }
 
+    void CorruptionVeins()
+    {
+        var positions = GetRandomPositions(veinTick, Vector2Int.zero, new Vector2Int(WorldController.Main.World.WidthInBlocks, WorldController.Main.World.LengthInBlocks));
+
+        foreach(var pos in positions)
+        {
+            if(Random.Range(0.0f,100) < veinChance || WorldController.Main.World.GetCorruption(pos.x,pos.y) < 1.0f)
+            {
+                continue;
+            }
+
+            int veinsPerNode = Random.Range(1, maxVeins);
+            int stepsPerVein = Random.Range(1, maxStepsPerVein);
+
+
+            for (int i = 0; i < veinsPerNode; i++)
+            {
+                Vector2Int prev = pos;
+                Vector2Int dir = Vector2Int.zero;
+                float curr = 1.0f;
+                int c = 0;
+                while (curr > 0 && c < stepsPerVein)
+                {
+                    curr -= Random.Range(0, 0.01f);
+                    Vector2Int next = prev + dir + new Vector2Int(Random.Range(-1, 2), Random.Range(-1, 2));
+                    if (prev == next || !IsValidPosition(next.x, next.y))
+                    {
+                        c++;
+                        continue;
+                    }
+
+                    var nextCorr = WorldController.Main.World.GetCorruption(next.x, next.y);
+                    WorldController.Main.World.SetCorruption(next.x, next.y, curr + nextCorr);
+
+                    dir = next - prev;
+                    dir /= (int)dir.magnitude;
+
+                    prev = next;
+                    c++;
+                }
+
+            }
+        }
+    }
 }
